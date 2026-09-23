2d8d37038aed62df8823c39a1e28b73ffd32725a
maxmatvi <maksym.matvi@gmail.com>
2026-09-23 09:42:54 +0200

    Question 5 commencée

---
+++ src/jeu/Sabot.java
@@ -0,0 +1,5 @@
+package jeu;
+
+public class Sabot {
+	private int nbCartes;
+}
---
+++ src/testsFonctionnels/TestJeuDeCartes.java
@@ -0,0 +1,11 @@
+package testsFonctionnels;
+
+import cartes.JeuDeCartes;
+
+public class TestJeuDeCartes {
+    public static void main(String[] args) {
+        JeuDeCartes jeu = new JeuDeCartes();
+        System.out.print(jeu.affichageJeuDeCartes());
+    }
+}
+
